af66c4a495cd20d778936ec7e45141eb87e34ea0
PetraMolenaar <petra.molenaar@student.hu.nl>
2025-11-28 21:25:23 +0100

    28-11-2025 Scripts geschreven om csv bestanden om te zetten in mtx

---
+++ scripts/03_06_5_data_omzetten_van_csv_naar_mtx.R
@@ -0,0 +1,24 @@
+library(Matrix)
+
+# Pad naar inputbestand
+input_file <- "/home/data/projecticum/splicing/data/e65_count_matrix.csv"
+
+# Pad naar output map
+outdir <- "~/data_integration/bewerkte_data"
+
+# Maak map aan als die nog niet bestaat
+if (!dir.exists(outdir)) {
+  dir.create(outdir, recursive = TRUE)
+}
+
+# Laad CSV (alleen de eerste 10 rijen voor voorbeeld)
+counts <- read.csv(input_file, row.names = 1)
+
+# Zet om naar numeric matrix
+counts_matrix <- as.matrix(counts)
+counts_matrix <- Matrix(counts_matrix, sparse = TRUE)
+
+writeMM(counts_matrix, file = file.path(outdir, "e65_count_matrix.mtx"))
+
+system("gzip -9 ~/data_integration/bewerkte_data/*.mtx")
+
---
+++ scripts/06_09_5_data_omzetten_van_csv_naar_mtx.R
@@ -0,0 +1,23 @@
+library(Matrix)
+
+# Pad naar inputbestand
+input_file <- "/home/data/projecticum/splicing/data/e95_count_matrix.csv.gz"
+
+# Pad naar output map
+outdir <- "~/data_integration/bewerkte_data"
+
+# Maak map aan als die nog niet bestaat
+if (!dir.exists(outdir)) {
+  dir.create(outdir, recursive = TRUE)
+}
+
+# Laad CSV (alleen de eerste 10 rijen voor voorbeeld)
+counts <- read.csv(input_file, row.names = 1)
+
+# Zet om naar numeric matrix
+counts_matrix <- as.matrix(counts)
+counts_matrix <- Matrix(counts_matrix, sparse = TRUE)
+
+writeMM(counts_matrix, file = file.path(outdir, "e95_count_matrix.mtx"))
+
+system("gzip -9 ~/data_integration/bewerkte_data/*.mtx")
