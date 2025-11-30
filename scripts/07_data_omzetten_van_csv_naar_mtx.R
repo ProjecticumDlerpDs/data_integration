e0e76788b94190c1736e207fbef6ea51beb8e352
PetraMolenaar <petra.molenaar@student.hu.nl>
2025-11-30 22:53:24 +0100

    30-11-2025 Script aangepast zodat de output overeen komt met de aparte scripts. Ook de uitleg uitgebreid om de stappen makkelijker te volgen.

--- scripts/07_data_omzetten_van_csv_naar_mtx.R
+++ scripts/07_data_omzetten_van_csv_naar_mtx.R
@@ -1,6 +1,7 @@
-library(Matrix)
+# Installeer indien nodig de Matrix package en laadt deze
+if (!require(Matrix, quietly = TRUE)) install.packages("Matrix"); library(Matrix)
 
-# Lijst van input CSV-bestanden
+# Geef het pad naar input CSV-bestanden aan
 input_files <- c(
   "/home/data/projecticum/splicing/data/e65_count_matrix.csv",
   "/home/data/projecticum/splicing/data/e75_count_matrix.csv.gz",
@@ -8,31 +9,38 @@ input_files <- c(
   "/home/data/projecticum/splicing/data/e95_count_matrix.csv.gz"
 )
 
-# Output map
-outdir <- "~/data_integration/bewerkte_data"
+# Geef het pad naar output map aan
+outdir <- path.expand ("~/data_integration/bewerkte_data")
 
-# Maak output map aan indien nodig
+# Maak output map aan indien deze nog niet bestaat
 if (!dir.exists(outdir)) {
   dir.create(outdir, recursive = TRUE)
 }
 
+# Zet csv bestand om in mtx
 convert_csv_to_mtx_gz <- function(input_file, outdir) {
-  # Basisnaam voor output
-  base <- gsub("\\.csv(\\.gz)?$", "", basename(input_file))
-  out_mtx_gz <- file.path(outdir, paste0(base, "_matrix.mtx.gz"))
   
-  # Laad CSV (R opent automatisch .gz)
+  # Maak een basisnaam aan voor de output, verwijder .csv of .csv.gz en zet dit om in .mtx.gz.
+  base <- gsub("\\.csv(\\.gz)?$", "", basename(input_file))
+  out_mtx_gz <- file.path(outdir, paste0(base, ".mtx.gz"))
+  
+  # Laad csv bestanden in, de eerste kolom wordt gebruikt als rij naam.
   counts <- read.csv(input_file, row.names = 1)
   
-  # Zet om naar numeric sparse matrix
+  # Zet csv bestanden om naar numeric sparse matrix, hierbij worden de 0-waarden niet opgeslagen wat zorgt voor minder grote bestanden.
   counts_matrix <- as.matrix(counts)
   counts_matrix <- Matrix(counts_matrix, sparse = TRUE)
   
-  # Schrijf direct naar .mtx.gz
-  gz_con <- gzfile(out_mtx_gz, "w")
-  writeMM(counts_matrix, file = gz_con)
-  close(gz_con)
+  # Maak pad naar tijdelijk .mtx bestand
+  out_mtx <- file.path(outdir, paste0(base, ".mtx"))
   
+  # Schrijf de sparse matrix naar .mtx
+  writeMM(counts_matrix, file = out_mtx)
+  
+  # Comprimeer achteraf met maximale gzip-compressie
+  system(paste("gzip -9 -f", shQuote(out_mtx)))
+  
+  # Geef een bericht wanneer een bestand klaar is
   message("Gemaakt: ", out_mtx_gz)
 }
 
